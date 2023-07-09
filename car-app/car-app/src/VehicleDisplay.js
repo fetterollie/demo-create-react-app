4aa4f7da50496809d1f49f8f708347dfc1602276
fetterollie <jonathan.fetterolf@gmail.com>
2023-07-09 15:47:45 -0400

    adding filtering functionality to postgres query results

--- car-app/car-app/src/VehicleDisplay.js
+++ car-app/car-app/src/VehicleDisplay.js
@@ -1,9 +1,11 @@
 import { Container } from "@material-ui/core";
 import ListCars from "./components/ListCars";
+import FilterCars from "./components/FilterCars";
 
 const VehicleDisplay = () => {
     return (
         <Container>
+            <FilterCars />
             <ListCars />
         </Container>
     );
