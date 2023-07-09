25e9a81796d830aaed1ccbb79c841614aaefc795
fetterollie <jonathan.fetterolf@gmail.com>
2023-07-09 15:02:10 -0400

    created functionality to list out cars

--- car-app/car-app/src/components/ListCars.js
+++ car-app/car-app/src/components/ListCars.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Container, Typography } from "@material-ui/core";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 const ListCars = () => {
 
@@ -21,11 +24,26 @@ const ListCars = () => {
         getCars();
     }, []);
 
+    //output
     console.log(cars);
 
     return (
         <Container>
             <Typography variant="h5">Car List</Typography>
+            {cars.map(car => (
+                <Card sx={{ width: "300px" }}>
+                    <CardContent>
+                        <Typography>
+                            <img 
+                                src={car.imgurl ? car.imgurl : "https://www.insticc.org/node/TechnicalProgram/56e7352809eb881d8c5546a9bbf8406e.png"}
+                                width='200px'
+                            />
+                        </Typography>
+                        <Typography variant="h6">{car.make ? car.make : "N/A"}</Typography>
+                        <Typography>{`Year: ${car.year ? car.year : "N/A"} Model: ${car.model ? car.model : "N/A"} Color: ${car.color ? car.color : "N/A"}`}</Typography>
+                    </CardContent>
+                </Card>
+            ))}
         </Container>
     );
 }
