1d0e5f03b5b1bec8d2f6daf23553a94c4605d824
fetterollie <jonathan.fetterolf@gmail.com>
2023-07-12 09:12:43 -0400

    centering home component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Weather from './components/Weather';
 import Clicker from './components/Clicker';
@@ -8,14 +9,39 @@ const Home = () => {
 
     return (
         <Container>
-            <Typography variant='h5'>
+            <Typography variant='h5' >
                 Home
             </Typography>
-            <Typography color="textSecondary" gutterBottom>
-                Lorem ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Typography>
-            <Weather />
-            <Clicker />
+            <Grid 
+                container 
+                xs={12}
+                spacing={12} 
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+            >
+                <Grid 
+                    item 
+                    xs={12} 
+                    spacing={12} 
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography color="textSecondary">
+                        Lorem ipsum  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Weather />
+                </Grid>
+                <Grid item xs={12}>
+                    <Clicker />
+                </Grid>
+            </Grid>
+        
         </Container>
     );
 }
